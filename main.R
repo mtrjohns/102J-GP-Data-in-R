cc21effbff065a7a731462904da4b834b5280727
Michael Johns <mtrj1776@hotmail.co.uk>
2021-02-05 01:08:46 +0000

    added database file to .gitignore

--- main.R
+++ main.R
@@ -1,1 +1,13 @@
+install.packages('tidyverse')
+install.packages('RPostgreSQL')
+
+
 print("Hello R")
+library(RPostgreSQL)
+library(tidyverse)
+
+# PostgreSQL server driver
+drv <- dbDriver('PostgreSQL')
+
+con <- dbConnect(drv, dbname='gp_practice_data', host='localhost', port='5432',
+                 user='postgres', password=.rs.askForPassword('Password:'))
---
+++ vashmds.R
@@ -0,0 +1,7 @@
+View(vashmds)
+totalRecords = nrow(vashmds)
+
+cat("Number of rows in vashmds = ", totalRecords)
+# 9406 Records
+
+head(vashmds)
