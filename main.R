a9a650c1cc873477bc0217139cb40d1b322cb62f
Michael Johns <mtrj1776@hotmail.co.uk>
2021-02-05 01:16:34 +0000

    Corrected port number to '5432' after remnant postgreSQL server detail removal

--- main.R
+++ main.R
@@ -14,5 +14,5 @@ drv <- dbDriver('PostgreSQL')
 #-- Removal ofprevious installation of postgreSQL versions
 #-- from program files/postgres and appdata/roaming/pgadmin
 #-- corrected connection refused issues
-con <- dbConnect(drv, dbname='gp_practice_data', host='localhost', port='5433',
+con <- dbConnect(drv, dbname='gp_practice_data', host='localhost', port='5432',
                  user='postgres', password=.rs.askForPassword('Password:'))
