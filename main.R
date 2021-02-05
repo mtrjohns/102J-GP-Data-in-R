af7986d3883556e28190db04b993425eaced3865
Michael Johns <mtrj1776@hotmail.co.uk>
2021-02-05 01:14:17 +0000

    # Connect to gp_practice_data PostgreSQL database #-- Error: Connection refused #-- Removal ofprevious installation of postgreSQL versions #-- from program files/postgres and appdata/roaming/pgadmin #-- corrected connection refused issues

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
-install.packages('tidyverse')
-install.packages('RPostgreSQL')
+#install.packages('tidyverse')
+#install.packages('RPostgreSQL')
 
 
 print("Hello R")
@@ -9,5 +9,10 @@ library(tidyverse)
 # PostgreSQL server driver
 drv <- dbDriver('PostgreSQL')
 
-con <- dbConnect(drv, dbname='gp_practice_data', host='localhost', port='5432',
+# Connect to gp_practice_data PostgreSQL database
+#-- Error: Connection refused
+#-- Removal ofprevious installation of postgreSQL versions
+#-- from program files/postgres and appdata/roaming/pgadmin
+#-- corrected connection refused issues
+con <- dbConnect(drv, dbname='gp_practice_data', host='localhost', port='5433',
                  user='postgres', password=.rs.askForPassword('Password:'))
