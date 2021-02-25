b6f49d131c4fcdafb713788be95084e8f5453e7b
Michael Johns <mtrj1776@gmail.com>
2021-02-25 13:09:31 +0000

    ConnectPostgreSQL.R tidy up. Attempt at combining function for local host. Will need to return two values. Possible future implementation option.

--- ConnectPostgreSQL.R
+++ ConnectPostgreSQL.R
@@ -1,53 +1,55 @@
+# -----------------------------------------------
+# PostgreSQL Database Connection Wrapper Class
+# author: Michael Johns
+#------------------------------------------------
+
 #install.packages('RPostgreSQL')
 
 library(RPostgreSQL)
 
-# Load the PostgreSQL server driver
-drv <- dbDriver('PostgreSQL')
+# Load database driver (driver)
+connectDriver <- function(inDriver){
+  drv <- dbDriver(inDriver)
+  return(drv)
+}
 
-# Connect to gp_practice_data PostgreSQL database
-connectDB <- function(inDbName, inPort, inHost)
+# Connect to database
+# (Driver, name, host address, port, database username)
+connectDB <- function(inDriver, inDbName, inHost, inPort, inUser)
 {
-  # Connect to gp_practice_data PostgreSQL database
   # Warning: .rs.askForPassword is a Rstudio only function
   #-- Connection error could occur: Connection refused (Windows 10 x64)
   #-- Removal of previous installation of PostgreSQL versions
   #-- from C:\Program Files\PostgreSQL
   #-- and C:\Users\<username>\AppData\Roaming\pgAdmin
   #-- corrected connection refused issues
-  con <- dbConnect(drv, inDbName,
-                   host=inHost, port=inPort,
-                   user='postgres',
-                   password=.rs.askForPassword('Database Password:'))
   
-  # close db connection after function call exits
-  on.exit(dbDisconnect(con))
+  # Connect to PostgreSQL database
+  con <- dbConnect(inDriver, inDbName,
+                   host=inHost, port=inPort,
+                   user=inUser,
+                   password=.rs.askForPassword('Database Password:'))
 
   return(con)
 }
 
-db <- connectDB('gp_practice_data', 5432, 'localhost')
 
-# Check all tables in the database
-dbListTables(con)
-
-# check columns of address table
-dbGetQuery(con, "
-           select column_name, 
-	         ordinal_position,
-           data_type,
-           character_maximum_length,
-           numeric_precision
-           from INFORMATION_SCHEMA.COLUMNS
-           where table_schema = 'public'
-           and table_name = 'address';")
-dbDisconnect(db)
+# Connect to PostgreSQL database on a localhost connection
+# (database name, database port, database username)
+connectPostgresLocalhost <- function(inDbName, inPort, inUser)
+{
+  database_driver <- connectDriver('PostgreSQL')
+  
+  database <- connectDB(database_driver, inDbName,
+                        'localhost', inPort,
+                        inUser)
+  return(database, database_driver)
+}
 
 # Disconnect and unload PostgreSQL driver
+# (Database connection, PostgreSQL driver)
 disconnect_Database <- function(connection, driver) {
   dbDisconnect(connection)
   dbListConnections(driver)
   dbUnloadDriver(driver)
 }
-disconnect_Database(db, drv)
-dbListConnections(drv)
--- main.R
+++ main.R
@@ -1,22 +1,65 @@
-source("ConnectPostgreSQL.R")
-  
+# -----------------------------------------------
+# Packages that require installation (uncomment to install)
+#------------------------------------------------
+
 #install.packages('tidyverse')
 
-# Connect to DB (Database name, port number, host address, database username)
-db <- connectDB('gp_practice_data', 5432, 'localhost', 'postgres')
+# -----------------------------------------------
+# Load required source files
+#------------------------------------------------
+
+source("ConnectPostgreSQL.R")
+  
+# -----------------------------------------------
+# Database connections and driver initialisation
+#------------------------------------------------
+
+# Load database driver (database_name)
+database_driver <- connectDriver('PostgreSQL')
+
+# Connect to DB (Driver, name, host address, port, database username)
+database <- connectDB(database_driver,
+                      'gp_practice_data',
+                      'localhost', 5432,
+                      'postgres')
+
+# Connect to DB (Driver, name, host address, port, database username)
+database <- connectPostgresLocalhost('gp_practice_data', 5432, 'postgres')
+  
+
+  
+# Ensure connections are always closed on function exit
+
 
 print("Hello R")
 library(tidyverse)
 
- # query a dataset ####
-surgery <- dbGetQuery(con, "select distinct a.practiceid 
-                      from address a
-                      join gp_data_up_to_2015 b
-                      on a.practiceid = b.practiceid;")
-View(surgery)
+# Check all tables in the database
+dbListTables(db)
 
-surgery <- sort(surgery$practiceid)
+# check columns of address table
+dbGetQuery(con, "
+           select column_name, 
+	         ordinal_position,
+           data_type,
+           character_maximum_length,
+           numeric_precision
+           from INFORMATION_SCHEMA.COLUMNS
+           where table_schema = 'public'
+           and table_name = 'address';")
 
-total_rows <- dbGetQuery(con, "select count(*) from public.gp_data_up_to_2015")
+# Query a dataset ####
+#surgery <- dbGetQuery(con, "select distinct a.practiceid 
+                      #from address a
+                      #join gp_data_up_to_2015 b
+                      #on a.practiceid = b.practiceid;")
+#View(surgery)
 
+#surgery <- sort(surgery$practiceid)
 
+#total_rows <- dbGetQuery(con, "select count(*) from public.gp_data_up_to_2015")
+
+# Disconnect database and driver
+dbListConnections(database_driver)
+disconnect_Database(database)
+dbListConnections(database_driver)
